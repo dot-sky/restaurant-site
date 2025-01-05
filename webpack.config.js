55ea957b1341ad977a4720f1c29c310d63f83ba5
dot-sky <108539845+dot-sky@users.noreply.github.com>
2025-01-05 08:37:36 -0400

    Install and configure HtmlWebpackPlugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 module.exports = {
   mode: "development",
@@ -7,4 +8,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
