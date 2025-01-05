6f1b46e505c6e2a4242dde0b92b7a293de2bfa0f
dot-sky <108539845+dot-sky@users.noreply.github.com>
2025-01-05 08:46:02 -0400

    Configure webpack dev server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const { watch } = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 module.exports = {
@@ -13,6 +14,10 @@ module.exports = {
       template: "./src/template.html",
     }),
   ],
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
   module: {
     rules: [
       {
