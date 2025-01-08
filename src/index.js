47909b983a535185e23356958a28aa9c05fe9296
dot-sky <108539845+dot-sky@users.noreply.github.com>
2025-01-08 07:11:46 -0400

    Add initial function to create homepage with JS.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,67 @@
 import "./styles.css";
 import { testVar } from "./module.js";
+import lunchImg from "./assets/tableware_128.png";
+import breakfastImg from "./assets/tea_128.png";
+import pastryImg from "./assets/croissant_128.png";
 
 console.log(testVar);
+
+function createHomepage(doc) {
+  const content = document.querySelector("#main");
+  const main = createHomepageElements(doc);
+  content.appendChild(main);
+  // fill
+  // add to main
+}
+function createHomepageElements(doc) {
+  //create elements
+  const mainContainer = document.createElement("div");
+  const heroContainer = document.createElement("div");
+  const cardSection = document.createElement("div");
+  const hoursSection = document.createElement("div");
+  const mapSection = document.createElement("div");
+
+  const heroTitle = document.createElement("h1");
+  const heroDesc = document.createElement("p");
+
+  const hoursImage = document.createElement("div");
+  const hoursContainer = document.createElement("div");
+
+  const mapContainer = document.createElement("div");
+  const mapDesc = document.createElement("div");
+  // fillContent
+  heroTitle.textContent = "Dreamer's Place";
+  heroDesc.textContent = "Your place for healthy and delicious food!";
+  const cardsImg = {
+    lunch: lunchImg,
+    breakfast: breakfastImg,
+    pastry: pastryImg,
+  };
+  // creating cards
+  for (let elem in cardsImg) {
+    const cardInfo = document.createElement("div");
+    const cardImg = document.createElement("img");
+    const cardDesc = document.createElement("p");
+
+    cardImg.setAttribute("src", cardsImg[elem]);
+    cardDesc.textContent = elem;
+
+    cardInfo.appendChild(cardImg);
+    cardInfo.appendChild(cardDesc);
+
+    cardSection.appendChild(cardInfo);
+    // console.log({ card, value: cards[card] });
+  }
+  const card = document.createElement("div");
+
+  // add
+  heroContainer.appendChild(heroTitle);
+  heroContainer.appendChild(heroDesc);
+
+  // adding to main container
+  mainContainer.appendChild(heroContainer);
+  mainContainer.appendChild(cardSection);
+
+  return mainContainer;
+}
+createHomepage(document);
