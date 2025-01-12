98cc2cdc97fbeb7f4fc07304c2cc9c8f2aa64c29
dot-sky <108539845+dot-sky@users.noreply.github.com>
2025-01-12 08:36:12 -0400

    Separate section creation to functions.

--- src/homePage.js
+++ src/homePage.js
@@ -4,189 +4,194 @@ import pastryImg from "./assets/croissant_128.png";
 import plate from "./assets/plate.jpg";
 import "./homePage.css";
 export function createHomePage(doc) {
-  const main = createHomepageElements(doc);
-  return main;
-  // fill
-  // add to main
+  const mainContainer = doc.createElement("div");
+  const { cardItems, days, map } = getDataObjects();
+
+  const heroContainer = createHeroSection(doc);
+  const cardSection = createCardsSection(doc, cardItems);
+  const hoursSection = createHoursSection(doc, days);
+  const mapSection = createMapSection(doc, map);
+
+  mainContainer.appendChild(heroContainer);
+  mainContainer.appendChild(cardSection);
+  mainContainer.appendChild(hoursSection);
+  mainContainer.appendChild(mapSection);
+
+  return mainContainer;
 }
-const imgPath = "./assets/";
-const cardItems = [
-  {
-    title: "Breakfast",
-    desc: "Feel-good mornings start here.",
-    img: lunchImg,
-  },
-  {
-    title: "Breakfast",
-    desc: "Feel-good mornings start here.",
-    img: breakfastImg,
-  },
-  {
-    title: "Breakfast",
-    desc: "Feel-good mornings start here.",
-    img: pastryImg,
-  },
-];
-const days = [
-  {
-    nameDay: "Monday",
-    hour: "7:00am to 10:00pm",
-  },
-  {
-    nameDay: "Tuesday",
-    hour: "7:00am to 10:00pm",
-  },
-  {
-    nameDay: "Wednesday",
-    hour: "7:00am to 10:00pm",
-  },
-  {
-    nameDay: "Thursday",
-    hour: "7:00am to 10:00pm",
-  },
-  {
-    nameDay: "Friday",
-    hour: "7:00am to 10:00pm",
-  },
-  {
-    nameDay: "Saturday",
-    hour: "7:30am to 9:00pm",
-  },
-  {
-    nameDay: "Sunday",
-    hour: "7:30am to 9:00pm",
-  },
-];
-const map = {
-  src: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3021.032573452489!2d-73.97717992428574!3d40.783298033248215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDDCsDQ2JzU5LjkiTiA3M8KwNTgnMjguNiJX!5e0!3m2!1sen!2sbo!4v1736680519434!5m2!1sen!2sbo",
-  width: "100%",
-  height: "450",
-  style: "border:0;",
-  allowfullscreen: "",
-  loading: "lazy",
-  referrerpolicy: "no-referrer-when-downgrade",
-};
-function createHomepageElements(doc) {
-  //create elements
-  const mainContainer = document.createElement("div");
+function getDataObjects() {
+  const cardItems = [
+    {
+      title: "Breakfast",
+      desc: "Feel-good mornings start here.",
+      img: lunchImg,
+    },
+    {
+      title: "Breakfast",
+      desc: "Feel-good mornings start here.",
+      img: breakfastImg,
+    },
+    {
+      title: "Breakfast",
+      desc: "Feel-good mornings start here.",
+      img: pastryImg,
+    },
+  ];
+  const days = [
+    {
+      nameDay: "Monday",
+      hour: "7:00am to 10:00pm",
+    },
+    {
+      nameDay: "Tuesday",
+      hour: "7:00am to 10:00pm",
+    },
+    {
+      nameDay: "Wednesday",
+      hour: "7:00am to 10:00pm",
+    },
+    {
+      nameDay: "Thursday",
+      hour: "7:00am to 10:00pm",
+    },
+    {
+      nameDay: "Friday",
+      hour: "7:00am to 10:00pm",
+    },
+    {
+      nameDay: "Saturday",
+      hour: "7:30am to 9:00pm",
+    },
+    {
+      nameDay: "Sunday",
+      hour: "7:30am to 9:00pm",
+    },
+  ];
+  const map = {
+    src: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3021.032573452489!2d-73.97717992428574!3d40.783298033248215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDDCsDQ2JzU5LjkiTiA3M8KwNTgnMjguNiJX!5e0!3m2!1sen!2sbo!4v1736680519434!5m2!1sen!2sbo",
+    width: "100%",
+    height: "450",
+    style: "border:0;",
+    allowfullscreen: "",
+    loading: "lazy",
+    referrerpolicy: "no-referrer-when-downgrade",
+  };
+  return { cardItems, days, map };
+}
+function createHomepageElements(doc) {}
+function createHeroSection(doc) {
+  const heroContainer = doc.createElement("div");
+  const heroTitle = doc.createElement("h1");
+  const heroDesc = doc.createElement("p");
 
-  const heroContainer = document.createElement("div");
-  const cardSection = document.createElement("div");
-  const hoursSection = document.createElement("div");
-  const mapSection = document.createElement("div");
-
-  const heroTitle = document.createElement("h1");
-  const heroDesc = document.createElement("p");
-
-  const hoursImageWrapper = document.createElement("div");
-  const hoursContainer = document.createElement("div");
-
-  const hoursTitle = document.createElement("h2");
-  const hoursDesc = document.createElement("p");
-  const daysList = document.createElement("ul");
-
-  const mapText = document.createElement("div");
-  const mapContainer = document.createElement("div");
-
-  const mapTitle = document.createElement("h2");
-  const mapAddress = document.createElement("p");
-  const mapDesc = document.createElement("p");
-  const iframeMap = document.createElement("iframe");
-
-  // fillContent
   heroTitle.textContent = "Dreamer's Place";
   heroDesc.textContent = "Your place for healthy and delicious food!";
 
-  hoursTitle.textContent = "Visit Us";
-  hoursDesc.textContent = "We are dedicated to give you the best experience";
-
-  mapTitle.textContent = "Find Us Here";
-  mapAddress.textContent = "5th Ave & 86th St, New York, NY 10028, USA.";
-  mapDesc.textContent =
-    "Located near Central Park, our restaurant is the perfect spot to enjoy a meal after exploring the city's iconic landmarks. Discover us here!";
-  // add classes
   heroContainer.classList.add("hero-container");
+
+  heroContainer.appendChild(heroTitle);
+  heroContainer.appendChild(heroDesc);
+  return heroContainer;
+}
+function createCardsSection(doc, cards) {
+  const cardSection = doc.createElement("div");
   cardSection.classList.add("card-section");
 
-  hoursSection.classList.add("section-50");
-  hoursImageWrapper.classList.add("img-hours-container");
-  hoursContainer.classList.add("hours");
-  daysList.classList.add("h5");
-  daysList.classList.add("text-light");
+  for (let item of cards) {
+    const cardItem = doc.createElement("div");
+    const img = doc.createElement("img");
+    const title = doc.createElement("p");
+    const desc = doc.createElement("p");
 
-  mapText.classList.add("map-text");
-  mapContainer.classList.add("map-container");
-  mapAddress.classList.add("text-primary");
-  mapSection.classList.add("section-50");
-  // creating cards
-  for (let item of cardItems) {
-    // DOM creation
-    const cardItem = document.createElement("div");
-    const cardImg = document.createElement("img");
-    const cardTitle = document.createElement("p");
-    const cardDesc = document.createElement("p");
-    // add class
+    title.textContent = item.title;
+    desc.textContent = item.desc;
+    img.setAttribute("src", item.img);
+
     cardItem.classList.add("card-item");
-    // content
-    cardImg.setAttribute("src", item.img);
-    cardTitle.textContent = item.title;
-    cardDesc.textContent = item.desc;
-    // add to card
-    cardItem.appendChild(cardImg);
-    cardItem.appendChild(cardTitle);
-    cardItem.appendChild(cardDesc);
-    // add to container
+
+    cardItem.appendChild(img);
+    cardItem.appendChild(title);
+    cardItem.appendChild(desc);
+
     cardSection.appendChild(cardItem);
   }
-  // creating hours section
-  const hoursImg = document.createElement("img");
+  return cardSection;
+}
+function createHoursSection(doc, daysObj) {
+  const hoursSection = doc.createElement("div");
+  const imgWrapper = doc.createElement("div");
+  const textContainer = doc.createElement("div");
+  const hoursImg = doc.createElement("img");
+  const title = doc.createElement("h2");
+  const desc = doc.createElement("p");
+  const daysList = doc.createElement("ul");
+
+  title.textContent = "Visit Us";
+  desc.textContent = "We are dedicated to give you the best experience";
   hoursImg.setAttribute("src", plate);
   hoursImg.setAttribute(
     "alt",
     "Two plates in a table, one of them containing a salad."
   );
 
-  // creating hours list
-  // add
-  for (let day of days) {
-    const li = document.createElement("li");
-    const spanPrimary = document.createElement("span");
+  for (let day of daysObj) {
+    const li = doc.createElement("li");
+    const spanPrimary = doc.createElement("span");
 
-    // classes
-    spanPrimary.classList.add("text-primary");
-    // content
     spanPrimary.textContent = day.nameDay;
     li.textContent = day.hour;
-    // add
+
+    spanPrimary.classList.add("text-primary");
+
     li.prepend(spanPrimary);
-    // add to ul
     daysList.appendChild(li);
   }
-  // modifying map iframe
-  for (const key in map) {
-    iframeMap.setAttribute(key, map[key]);
+
+  hoursSection.classList.add("section-50");
+  imgWrapper.classList.add("img-hours-container");
+  textContainer.classList.add("hours");
+  daysList.classList.add("h5");
+  daysList.classList.add("text-light");
+
+  imgWrapper.appendChild(hoursImg);
+  textContainer.appendChild(title);
+  textContainer.appendChild(desc);
+  textContainer.appendChild(daysList);
+
+  hoursSection.appendChild(imgWrapper);
+  hoursSection.appendChild(textContainer);
+  return hoursSection;
+}
+
+function createMapSection(doc, mapObj) {
+  const mapSection = doc.createElement("div");
+  const textContainer = doc.createElement("div");
+  const mapContainer = doc.createElement("div");
+  const title = doc.createElement("h2");
+  const address = doc.createElement("p");
+  const desc = doc.createElement("p");
+  const iframeM = doc.createElement("iframe");
+
+  title.textContent = "Find Us Here";
+  address.textContent = "5th Ave & 86th St, New York, NY 10028, USA.";
+  desc.textContent =
+    "Located near Central Park, our restaurant is the perfect spot to enjoy a meal after exploring the city's iconic landmarks. Discover us here!";
+  // adding attributes to iframe
+  for (const key in mapObj) {
+    iframeM.setAttribute(key, mapObj[key]);
   }
-  // hero
-  heroContainer.appendChild(heroTitle);
-  heroContainer.appendChild(heroDesc);
-  // hours
-  hoursImageWrapper.appendChild(hoursImg);
-  hoursContainer.appendChild(hoursTitle);
-  hoursContainer.appendChild(hoursDesc);
-  hoursContainer.appendChild(daysList);
-  hoursSection.appendChild(hoursImageWrapper);
-  hoursSection.appendChild(hoursContainer);
-  // map
-  mapText.appendChild(mapTitle);
-  mapText.appendChild(mapAddress);
-  mapText.appendChild(mapDesc);
-  mapContainer.appendChild(iframeMap);
-  mapSection.appendChild(mapText);
+
+  textContainer.classList.add("map-text");
+  mapContainer.classList.add("map-container");
+  address.classList.add("text-primary");
+  mapSection.classList.add("section-50");
+
+  textContainer.appendChild(title);
+  textContainer.appendChild(address);
+  textContainer.appendChild(desc);
+  mapContainer.appendChild(iframeM);
+
+  mapSection.appendChild(textContainer);
   mapSection.appendChild(mapContainer);
-  // adding to main container
-  mainContainer.appendChild(heroContainer);
-  mainContainer.appendChild(cardSection);
-  mainContainer.appendChild(hoursSection);
-  mainContainer.appendChild(mapSection);
-  return mainContainer;
+  return mapSection;
 }
