0ac24b2aff182e23e8316f733ba86e36f691f2ae
dot-sky <108539845+dot-sky@users.noreply.github.com>
2025-01-13 06:59:29 -0400

    Delete comments.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import { createMenuPage } from "./menuPage.js";
 import { createAboutPage } from "./aboutPage.js";
 
 const pageController = (function (doc) {
-  // DOM selection
   const homeBtn = doc.querySelector("button#home-btn");
   const menuBtn = doc.querySelector("button#menu-btn");
   const aboutBtn = doc.querySelector("button#about-btn");
   const contentContainer = doc.querySelector("div#content");
   let selectedBtn = homeBtn;
 
-  // pages creation
   const homePage = createHomePage(doc);
   const menuPage = createMenuPage(doc);
   const aboutPage = createAboutPage(doc);
